9b47f7a4bd8e85212ad0fd5c892e019adf72152f
Justin <justin.vanvoorst@motili.com>
2020-11-06 13:07:17 -0700

    add to food-items

--- data/food-items.js
+++ data/food-items.js
@@ -1,16 +1,16 @@
 module.exports = [
     {
         id: 1,
-        item: 'Pumpkins',
+        item: 'Pumpkin',
         image:
             'https://images.unsplash.com/photo-1443464812268-44d8bb5f2df5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2048&q=80',
         description:
-            'A pumpkin is a cultivar of winter squash that is round with smooth, slightly ribbed skin, and is most often deep yellow to orange in coloration. The thick shell contains the seeds and pulp.',
+            'A pumpkin is a cultivar of winter squash that is round with smooth, slightly ribbed skin, and is most often deep yellow to orange in coloration.',
         price: 12,
     },
     {
         id: 2,
-        item: 'Avocados',
+        item: 'Avocado',
         image:
             'https://images.unsplash.com/photo-1449339854873-750e6913301b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
         description:
@@ -19,53 +19,74 @@ module.exports = [
     },
     {
         id: 3,
-        item: 'Oranges',
+        item: 'Orange',
         image:
             'https://images.unsplash.com/photo-1562599938-53b269c207a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=935&q=80',
         description:
-            'The orange is the fruit of various citrus species in the family Rutaceae; it primarily refers to Citrus × sinensis, which is also called sweet orange, ',
+            'The orange is the fruit of various citrus species in the family Rutaceae; it primarily refers to Citrus × sinensis.',
         price: 2.5,
     },
-    // {
-    //     id: 1,
-    //     image: 'url',
-    //     description: '',
-    //     price: 1.5
-    // },
-    // {
-    //     id: 1,
-    //     image: 'url',
-    //     description: '',
-    //     price: 1.5
-    // },
-    // {
-    //     id: 1,
-    //     image: 'url',
-    //     description: '',
-    //     price: 1.5
-    // },
-    // {
-    //     id: 1,
-    //     image: 'url',
-    //     description: '',
-    //     price: 1.5
-    // },
-    // {
-    //     id: 1,
-    //     image: 'url',
-    //     description: '',
-    //     price: 1.5
-    // },
-    // {
-    //     id: 1,
-    //     image: 'url',
-    //     description: '',
-    //     price: 1.5
-    // },
-    // {
-    //     id: 1,
-    //     image: 'url',
-    //     description: '',
-    //     price: 1.5
-    // }
+    {
+        id: 4,
+        item: 'Radish',
+        image:
+            'https://images.unsplash.com/photo-1587482990911-773a2aef47dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        description:
+            'The radish is an edible root vegetable of the family Brassicaceae that was domesticated in Asia prior to Roman times.',
+        price: 1.75,
+    },
+    {
+        id: 6,
+        item: 'Lemon',
+        image:
+            'https://images.unsplash.com/photo-1582979512210-99b6a53386f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
+        description:
+            'The lemon, Citrus limon, is a species of small evergreen tree in the flowering plant family Rutaceae, native to South Asia.',
+        price: 1.25,
+    },
+    {
+        id: 5,
+        item: 'Cucumber',
+        image:
+            'https://images.unsplash.com/photo-1449300079323-02e209d9d3a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80',
+        description:
+            'Cucumber is a widely-cultivated creeping vine plant in the Cucurbitaceae gourd family that bears cucumiform fruits.',
+        price: 1.15,
+    },
+    {
+        id: 8,
+        item: 'Carrot',
+        image:
+            'https://images.unsplash.com/photo-1579446155503-f01a57693728?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80l',
+        description:
+            'The carrot is a root vegetable, usually orange in color, though purple, black, red, white, and yellow cultivars exist.',
+        price: 1,
+    },
+    {
+        id: 7,
+        item: 'Lime',
+        image:
+            'https://images.unsplash.com/photo-1463123081488-789f998ac9c4?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+        description:
+            'A lime is a citrus fruit, which is typically round, green in color, 3–6 centimetres in diameter, and contains acidic juice vesicles.',
+        price: 1,
+    },
+    {
+        id: 9,
+        item: 'Mango',
+        image:
+            'https://images.unsplash.com/photo-1501746877-14782df58970?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80',
+        description:
+            'A mango is a stone fruit produced from numerous species of tropical trees belonging to the flowering plant genus Mangifera.',
+        price: 4.5,
+    },
+    {
+        id: 10,
+        item: 'Papaya',
+        image:
+            'https://images.unsplash.com/photo-1603431776073-c91a8ede374a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
+        description:
+            'The papaya, papaw, or pawpaw is the plant Carica papaya, one of the 22 accepted species in the genus Carica of the family Caricaceae.',
+        price: 3.5,
+    },
 ];
